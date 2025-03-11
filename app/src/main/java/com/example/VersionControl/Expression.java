86c4c3e978ea268dd78573743dbd7c043a07b5f3
Faisal Alsajjan <fta.sajjan@gmail.com>
2025-03-11 15:34:04 +0300

    feat: added comments

--- app/src/main/java/com/example/VersionControl/Expression.java
+++ app/src/main/java/com/example/VersionControl/Expression.java
@@ -1,9 +1,13 @@
 package com.example.VersionControl;
 
 public class Expression {
+    // left hand side of the expression >1 + 1
     public float lhs;
+    // right hand side of the expression 1 + >1
     public float rhs;
+    // operator of the expression 1 >+ 1
     public String operator;
+    // calculates the result of the expression
     public String evaluate(){
         return "Evaluation not implemented";
     }
--- app/src/main/java/com/example/VersionControl/ExpressionManager.java
+++ app/src/main/java/com/example/VersionControl/ExpressionManager.java
@@ -6,21 +6,27 @@ import java.util.regex.Pattern;
 public class ExpressionManager {
     public static Expression parse(String value) throws InvalidExpressionException{
         Expression exp = new Expression();
+        // find which operator
         for(var i : operators){
             if(value.contains(i)){
                 exp.operator=i;
                 break;
             }
         }
+        // no operator was found
         if(exp.operator== null){
             throw new InvalidExpressionException("No operator found");
         }
+        
+        // split around operator making "1 + 1" = new String[] {1, 1}
         String[] split = value.split(Pattern.quote(exp.operator));
+        // set the left hand side of the expression
         try{
-            exp.rhs = Float.parseFloat(split[0]);
+            exp.lhs = Float.parseFloat(split[0]);
         }catch(NumberFormatException nf){
             throw new InvalidExpressionException("Expected a number, found \""+split[0]+"\"");
         }
+        // set the right hand side of the expression
         exp.lhs = Float.parseFloat(split[1]);
         return exp;
     }
